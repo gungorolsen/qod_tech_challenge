f5b76c3924a0305c46729b171d39566b1504f690
gungorolsen <110291240+gungorolsen@users.noreply.github.com>
2022-07-31 07:54:12 +1000

    init

---
+++ qod_tech_challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qod_tech_challenge
+//
+//  Created by Oguzhan Gungor on 31/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
